732c71d0a85bab83acd52e5c4db305ac1247ae26
Max Maischein <corion@corion.net>
2023-01-20 18:17:38 +0100

    Rough outline, need a DB for Mojolicious

--- bin/serve.pl
+++ bin/serve.pl
@@ -1,24 +1,17 @@
 #!perl
 use strict;
 use warnings;
-use Mojolicious::Lite;
+use Mojolicious::Lite -signatures;
 use Mojo::JSON qw(decode_json encode_json);
 use Mojo::File;
+use MojoX::Session; # because we need a central session storage, not client-side
 use charnames ':full';
 
+use Authen::OATH;
+
 use FindBin;
-use lib "$FindBin::Bin/../../Weather-MOSMIX/lib";
-use Weather::MOSMIX;
 
 push @{app->static->paths} => "$FindBin::Bin/../public";
-
-my $dbfile = app->config->{dbfile}
-           || '../../Weather-MOSMIX/mosmix-forecast.sqlite';
-
-$dbfile = Mojo::File->new($dbfile);
-if( !$dbfile->is_abs ) {
-    $dbfile = Mojo::File->new("$FindBin::Bin/$dbfile");
-};
 
 # If we have a precompressed resource, serve that
 app->static->with_roles('+Compressed');
@@ -26,41 +19,92 @@ app->static->with_roles('+Compressed');
 # Compress all dynamic resources as well
 plugin 'Gzip';
 
-my $w = Weather::MOSMIX->new(
-    dbh => {
-        dsn => "dbi:SQLite:dbname=$dbfile",
-    },
+my %credentials = (
+    'demo' => { name => 'demo', password => 'demo', otp_secret => 'demo', },
 );
+
+plugin session => {store => 'dummy', expires_delta => 60*60};
 
 get '/' => sub {
     my $c = shift;
     return $c->redirect_to('index.html');
 };
 
-post '/forecast' => sub {
+get '/index' => sub {
     my $c = shift;
-    my $query = decode_json( $c->req->body );
-    my @locations = @{ $query->{'locations'} };
+	
+    my $session = $c->stash('session');
+	my $username;
 
-    my @res;
-    for my $l (@locations) {
-        my $f =
-            $w->forecast_dbh(latitude => $l->{latitude}, longitude => $l->{longitude} );
-        my $out = $w->format_forecast_dbh( $f, 6 );
-        #$out->[0]->{utf8} = "\N{CHECK MARK}";
-        push @res, $out;
-    };
-    my $res = { forecast => \@res };
-    $c->render( json => $res );
+    # Check if we already have a session
+    if ($session->load) {
+		$username = $session->data('username');
+	}
+    return $c->render('index', username => $username );
 };
 
-# This is available as a static file already
-#get '/locations.json' => sub {
-#    my $c = shift;
-#    my $res = $w->locations();
-#    $c->render( json => $res );
-#};
+sub validate_password( $username, $expected, $password ) {
+	return $expected eq $password
+}
 
+sub validate_totp( $username, $ts, $secret, $totp ) {
+	my $totp_alg = Authen::OATH->new($secret);
+	return $totp_alg->totp($ts) == $totp
+}
+
+sub valid_login( $username, $credential_type, $credential ) {
+	if( my $cred = $credentials{ $username } ) {
+		if( my $expected = $cred->{ $credential_type } ) {
+			if( $credential_type eq 'password' ) {
+				return validate_password( $username, $expected, $credential )
+			} elsif ( $credential_type eq 'totp' ) {
+				my $ts = time();
+				return validate_totp( $username, $ts, $expected, $credential )
+			} else {
+				return undef
+			}
+		}
+	} else {
+		return undef
+	}
+}
+
+get '/login' => sub( $c ) {
+    my $session = $c->stash('session');
+	# create a fresh session if necessary
+	$session->load or $session->create;
+	$session->flush;
+    return $c->render('login');
+};
+
+post '/login' => sub( $c ) {
+	my $target = $c->param('target') // 'index.html';
+	my $account = $c->param('account');
+	my $cred_type = $c->param('credential_type');
+	my $credential = $c->param('credential');
+
+    my $session = $c->stash('session');
+    $session->load or $session->create; # well, we should have a session here?!
+	
+	my $next = $c->url_for('/login', { target => $target });
+	if( ! $account ) {
+	} elsif( valid_login( $account, $cred_type, $credential )) {
+		$session->data( username => $account );
+	}
+	
+    $c->redirect( $target );
+};
 
 # Start the Mojolicious command system
 app->start;
+
+__DATA__
+@@ index.html.ep
+<!DOCTYPE html>
+<html>
+% if( defined $username ) {
+	<p>Welcome <%= $username %></p>
+% } else {
+	<p>Please <a href="login.html">log in</a></p>
+% }
+</html>
